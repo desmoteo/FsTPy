7ff26ca9693915b33805501db28398d9614070ef
Matteo Ferrabone <matteo.ferrabone@gmail.com>
2020-11-08 18:29:53 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setuptools.setup(
     name="fstpy",
-    version="0.0.5",
+    version="0.0.6",
     author="Matteo Ferrabone",
     author_email="matteo.ferrabone@gmail.com",
     license='MIT',
