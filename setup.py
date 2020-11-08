97c9c3df02ee39dada3c91dad18bfe623ab3f76e
Matteo Ferrabone <matteo.ferrabone@gmail.com>
2020-11-08 01:20:30 +0100

    Updated classifiers

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ import setuptools
 from os import path
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()    
+    long_description = f.read()
 
 setuptools.setup(
-    name="fstpy", 
-    version="0.0.3",
+    name="fstpy",
+    version="0.0.4",
     author="Matteo Ferrabone",
     author_email="matteo.ferrabone@gmail.com",
     license='MIT',
@@ -19,15 +19,24 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     scripts=['scripts/fstpyd'],
     install_requires=[
-      'fs',
-      'pyftpdlib',
-      'begins',
-  ],
+        'fs',
+        'pyftpdlib',
+        'begins',
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Information Technology',
+        'Topic :: Internet :: File Transfer Protocol (FTP)',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Filesystems',
+        'Topic :: Utilities ',
+        'Topic :: System',
+        'Topic :: System :: Archiving',
+        'Topic :: Internet :: File Transfer Protocol (FTP)'
     ],
     python_requires='>=3.6',
 )
